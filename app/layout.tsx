d48f769ce17c54e466830c8fc0de8103ca52303e
KazooTTT <yjwang.echo@gmail.com>
2023-10-14 18:55:04 +0800

    feat: home page init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import FullPageBackground from "@/components/FullPageBackground";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Mgclub Share Card',
-  description: 'input mgclub post url, output the long pic to share.',
-}
+  title: "Mgclub Share Card",
+  description: "input mgclub post url, output the long pic to share.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <>
+          <main>{children}</main>
+        </>
+      </body>
     </html>
-  )
+  );
 }
