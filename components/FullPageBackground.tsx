ce1c0a4b3aee4e8f164d5b6d9818c798132d5ac1
KazooTTT <yjwang.echo@gmail.com>
2023-10-14 19:16:03 +0800

    fix: not set the width and height

--- components/FullPageBackground.tsx
+++ components/FullPageBackground.tsx
@@ -4,32 +4,11 @@ import Image from "next/image";
 import React from "react";
 
 const FullPageBackground = () => {
-  // get the width and height of the viewport
-  const [viewportWidth, setViewportWidth] = React.useState(0);
-  const [viewportHeight, setViewportHeight] = React.useState(0);
-
-  React.useEffect(() => {
-    if (window) {
-      const handleResize = () => {
-        setViewportWidth(window.innerWidth);
-        setViewportHeight(window.innerHeight);
-      };
-
-      window.addEventListener("resize", handleResize);
-
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
-  }, []);
-
   return (
     <Image
       className="absolute inset-0 w-full h-full object-cover blur-md mix-blend-multiply filter brightness-50"
       alt="main background image"
       src="/background.jpeg"
-      width={viewportWidth}
-      height={viewportHeight}
     />
   );
 };
