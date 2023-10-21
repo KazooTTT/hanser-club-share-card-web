c53ca63405d73b935c8e92966aaddefc0d5ca0d0
KazooTTT <yjwang.echo@gmail.com>
2023-10-21 13:03:08 +0800

    fix : run pptr on vercel

--- utils/getPic.ts
+++ utils/getPic.ts
@@ -1,9 +1,25 @@
-// import pptr
-import pptr from "puppeteer";
+import pptr from "puppeteer-core";
+import chrome from "chrome-aws-lambda";
 
 const getPicFromURL = async (url: string) => {
-  // open url in the pptr
-  const browser = await pptr.launch();
+  const options = process.env.AWS_REGION
+    ? {
+        args: chrome.args,
+        executablePath: await chrome.executablePath,
+        headless: chrome.headless,
+      }
+    : {
+        args: [],
+        executablePath:
+          process.platform === "win32"
+            ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+            : process.platform === "linux"
+            ? "/usr/bin/google-chrome"
+            : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+      };
+
+  let browser = null;
+  browser = await pptr.launch(options);
   const page = await browser.newPage();
   await page.goto(url, {
     waitUntil: "networkidle0",
@@ -29,9 +45,15 @@ const getPicFromURL = async (url: string) => {
       quality: 100,
       captureBeyondViewport: true,
     });
-    await browser.close();
-    return image;
   }
+
+  await page.close();
+
+  browser.on("disconnected", () => {
+    console.log("浏览器断开连接");
+    //断开后设为 falsy 值
+    browser = null;
+  });
 
   return image;
 };
