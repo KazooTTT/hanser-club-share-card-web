1122cd5d280dfdbb90802357c289db5ae8a66a8f
KazooTTT <yjwang.echo@gmail.com>
2023-10-21 13:57:56 +0800

    fix: wrong version

--- utils/getPic.ts
+++ utils/getPic.ts
@@ -6,7 +6,7 @@ async function getBrowser() {
     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v117.0.0/chromium-v116.0.0-pack.tar`,
+      `https://github.com/Sparticuz/chromium/releases/download/v117.0.0/chromium-v117.0.0-pack.tar`,
     ),
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
