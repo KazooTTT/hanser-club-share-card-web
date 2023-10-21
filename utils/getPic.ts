7443bd32e99aba7b0ce44669a15986eaf7f33e2e
KazooTTT <yjwang.echo@gmail.com>
2023-10-21 17:33:31 +0800

    feat: change config

--- utils/getPic.ts
+++ utils/getPic.ts
@@ -1,26 +1,36 @@
 import chromium from "@sparticuz/chromium-min";
-import puppeteer from "puppeteer-core";
+import puppeteer, { Page } from "puppeteer-core";
 
 async function getBrowser() {
   return puppeteer.launch({
     args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath(
-      `https://github.com/Sparticuz/chromium/releases/download/v117.0.0/chromium-v117.0.0-pack.tar`,
+      `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`,
     ),
     headless: chromium.headless,
     ignoreHTTPSErrors: true,
   });
 }
 
+let _page: Page;
+async function getPage() {
+  if (_page) return _page;
+
+  const browser = await getBrowser();
+  _page = await browser.newPage();
+  return _page;
+}
+
 const getPicFromURL = async (url: string) => {
   // open url in the pptr
-  let browser = await getBrowser();
 
-  const page = await browser.newPage();
-  await page.goto(url);
+  const page = await getPage();
+  await page.goto(url, {
+    waitUntil: "domcontentloaded",
+  });
+
   await page.setViewport({ width: 390, height: 844, deviceScaleFactor: 2 });
-  await page.content();
 
   await page.addStyleTag({
     content: ".layout-header {display:none !important}",
